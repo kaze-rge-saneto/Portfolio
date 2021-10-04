9596dc7e1f86e63cf07c8422b30cea3d016b701a
saneto <kaze.rge@icloud.com>
2021-10-04 22:44:38 +0900

    ポートフォリオ

---
+++ copy.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://img.icons8.com/carbon-copy/100/000000/track-and-field.png">
+    <link rel="stylesheet" href="/Portfolio/css/style.css">
+    <title>模写コーディング</title>
+</head>
+<body class="copy-list">
+    <!--模写一覧-->
+    <div class="grid">
+        <!--カフェ-->
+        <div class="WCB">
+            <h3>カフェのサイトです。</h3>
+            <!--本のリンクをはる画像も？-->
+            <div class="WCB-image">
+                <a href="/WCBCafe/index.php" target="_blank"><img src="/Portfolio/image/WCBcafe.png" alt=""></a>
+            </div>
+        </div>
+
+        <!--カフェ-->
+        <div class="WCB">
+            <h3>カフェのサイトです。</h3>
+            <!--本のリンクをはる画像も？-->
+            <div class="WCB-image">
+                <img src="/Portfolio/image/WCBcafe.png" alt="">
+            </div>
+        </div>
+
+    </div>
+    <!--indexへ-->
+    <div class="index">
+        <a href="/Portfolio/index.php">戻る</a>
+    </div>
+
+</body>
+</html>
